476ddcb01fb4d9b97709d0b471147d834392bc54
Otavio Silva <osilva.everify@digio.com.br>
2021-12-29 09:50:15 -0300

    login project

---
+++ login/lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SignUpApp());
+}
+
+class SignUpApp extends StatelessWidget {
+  const SignUpApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(routes: {
+      '/': (context) => const SignUpScreen(),
+      '/welcome': (context) => const WelcomeScreen()
+    });
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController = TextEditingController();
+  final _usernameTextController = TextEditingController();
+
+  double _formProgress = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        LinearProgressIndicator(
+          value: _formProgress,
+        ),
+        Text(
+          'Sign up',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _firstNameTextController,
+            decoration: const InputDecoration(hintText: 'First Name'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _lastNameTextController,
+            decoration: const InputDecoration(hintText: 'Last Name'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _usernameTextController,
+            decoration: const InputDecoration(hintText: 'Username'),
+          ),
+        ),
+        TextButton(
+            onPressed: _showWelcomeScreen,
+            child: const Text('Sign up'),
+            style: ButtonStyle(foregroundColor:
+                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+              return states.contains(MaterialState.disabled)
+                  ? null
+                  : Colors.white;
+            }), backgroundColor:
+                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+              return states.contains(MaterialState.disabled)
+                  ? null
+                  : Colors.blue;
+            })))
+      ],
+    ));
+  }
+
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome',
+          style: Theme.of(context).textTheme.headline2,
+        ),
+      ),
+    );
+  }
+}
