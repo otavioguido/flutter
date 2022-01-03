228ca2b7feccae89ac7058ae89992cceeca01c9f
Otavio Silva <osilva.everify@digio.com.br>
2022-01-03 11:59:47 -0300

    added progress bar/validation to sign up project

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -53,6 +53,7 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
+        onChanged: _updateFormProgress,
         child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -85,7 +86,7 @@ class _SignUpFormState extends State<SignUpForm> {
           ),
         ),
         TextButton(
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
             style: ButtonStyle(foregroundColor:
                 MaterialStateProperty.resolveWith((Set<MaterialState> states) {
@@ -104,6 +105,25 @@ class _SignUpFormState extends State<SignUpForm> {
 
   void _showWelcomeScreen() {
     Navigator.of(context).pushNamed('/welcome');
+  }
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
   }
 }
 
