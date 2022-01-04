61cf498cf8ba3307d5b3fa753e0fa710e86548ef
Otavio Silva <osilva.everify@digio.com.br>
2022-01-04 11:41:54 -0300

    finished sign in project by adding animation completion

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -55,52 +55,51 @@ class _SignUpFormState extends State<SignUpForm> {
     return Form(
         onChanged: _updateFormProgress,
         child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        LinearProgressIndicator(
-          value: _formProgress,
-        ),
-        Text(
-          'Sign up',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextFormField(
-            controller: _firstNameTextController,
-            decoration: const InputDecoration(hintText: 'First Name'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextFormField(
-            controller: _lastNameTextController,
-            decoration: const InputDecoration(hintText: 'Last Name'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextFormField(
-            controller: _usernameTextController,
-            decoration: const InputDecoration(hintText: 'Username'),
-          ),
-        ),
-        TextButton(
-            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
-            child: const Text('Sign up'),
-            style: ButtonStyle(foregroundColor:
-                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
-              return states.contains(MaterialState.disabled)
-                  ? null
-                  : Colors.white;
-            }), backgroundColor:
-                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
-              return states.contains(MaterialState.disabled)
-                  ? null
-                  : Colors.blue;
-            })))
-      ],
-    ));
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedProgressIndicator(value: _formProgress),
+            Text(
+              'Sign up',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _firstNameTextController,
+                decoration: const InputDecoration(hintText: 'First Name'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _lastNameTextController,
+                decoration: const InputDecoration(hintText: 'Last Name'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _usernameTextController,
+                decoration: const InputDecoration(hintText: 'Username'),
+              ),
+            ),
+            TextButton(
+                onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
+                child: const Text('Sign up'),
+                style: ButtonStyle(foregroundColor:
+                    MaterialStateProperty.resolveWith(
+                        (Set<MaterialState> states) {
+                  return states.contains(MaterialState.disabled)
+                      ? null
+                      : Colors.white;
+                }), backgroundColor: MaterialStateProperty.resolveWith(
+                    (Set<MaterialState> states) {
+                  return states.contains(MaterialState.disabled)
+                      ? null
+                      : Colors.blue;
+                })))
+          ],
+        ));
   }
 
   void _showWelcomeScreen() {
@@ -140,5 +139,62 @@ class WelcomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class AnimatedProgressIndicator extends StatefulWidget {
+  const AnimatedProgressIndicator({Key? key, required this.value})
+      : super(key: key);
+
+  final double value;
+
+  @override
+  _AnimatedProgressIndicatorState createState() =>
+      _AnimatedProgressIndicatorState();
+}
+
+class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double> _curveAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) => LinearProgressIndicator(
+        value: _curveAnimation.value,
+        valueColor: _colorAnimation,
+        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: Duration(milliseconds: 1200), vsync: this);
+
+    final colorTween = TweenSequence([
+      TweenSequenceItem(
+          tween: ColorTween(begin: Colors.red, end: Colors.orange), weight: 1),
+      TweenSequenceItem(
+          tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
+          weight: 1),
+      TweenSequenceItem(
+          tween: ColorTween(begin: Colors.yellow, end: Colors.green),
+          weight: 1),
+    ]);
+
+    _colorAnimation = _controller.drive(colorTween);
+    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
+  }
+
+  @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.animateTo(widget.value);
   }
 }
